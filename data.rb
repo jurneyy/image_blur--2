8a5619b5b92ed057e2c11593ac328ea61612ae1a
Erny <erny@E-4.local>
2019-07-30 19:52:32 -0600

    adding data.rb

---
+++ data.rb
@@ -0,0 +1,28 @@
+class Image 
+
+	def initialize(data)
+
+		@information = data
+
+	end
+
+	def output_image
+
+		@information.each do |x|
+
+			print x.join("")
+			print "\n"
+		end
+
+	end
+
+end 
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
